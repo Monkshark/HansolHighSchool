62f6a5e65a180563e30bd19d4f06e61bcb521264
HuiDo Choo <manner0814@sjhansol.sjeduhs.kr>
2023-09-13 19:54:43 +0900

    Update

---
+++ app/src/main/java/com/ProG/HansolHighSchool/Data/SpinnerData.java
@@ -0,0 +1,8 @@
+package com.ProG.HansolHighSchool.Data;
+
+public class SpinnerData {
+
+    public static int spinnerGrade = 0;
+    public static int spinnerClass = 5;
+
+}
--- app/src/main/java/com/ProG/HansolHighSchool/Fragment/HomeFragment.java
+++ app/src/main/java/com/ProG/HansolHighSchool/Fragment/HomeFragment.java
@@ -19,7 +19,9 @@ import com.ProG.HansolHighSchool.API.getMealData;
 import com.ProG.HansolHighSchool.API.getTimetableData;
 import com.ProG.HansolHighSchool.Activity.AccountInfoActivity;
 import com.ProG.HansolHighSchool.Activity.LoginActivity;
+import com.ProG.HansolHighSchool.Activity.SettingsActivity;
 import com.ProG.HansolHighSchool.Data.LoginData;
+import com.ProG.HansolHighSchool.Data.SpinnerData;
 import com.ProG.HansolHighSchool.R;
 
 import java.text.SimpleDateFormat;
@@ -27,11 +29,14 @@ import java.util.Date;
 
 public class HomeFragment extends Fragment {
 
-    Button btn_hansolhs, btn_riroschool, btn_account;
-    TextView tv_meal, tv_timetable;
-    Date currentDate = new Date();
+    Button btn_hansolhs, btn_riroschool, btn_account, btn_setting;
+    @SuppressLint("StaticFieldLeak")
+    static TextView tv_meal;
+    @SuppressLint("StaticFieldLeak")
+    static TextView tv_timetable;
+    static Date currentDate = new Date();
     @SuppressLint("SimpleDateFormat")
-    private SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMdd");
+    private static SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMdd");
 
     @SuppressLint("SetTextI18n")
     @Override
@@ -41,10 +46,16 @@ public class HomeFragment extends Fragment {
         btn_hansolhs = view.findViewById(R.id.btn_hansolhs);
         btn_riroschool = view.findViewById(R.id.btn_riroschool);
         btn_account = view.findViewById(R.id.btn_account);
+        btn_setting = view.findViewById(R.id.btn_setting);
         tv_meal = view.findViewById(R.id.tv_meal);
         tv_timetable = view.findViewById(R.id.tv_timetable);
 
         String crdate = dateFormat.format(currentDate);
+
+        btn_setting.setOnClickListener(v -> {
+            Intent intentActivity = new Intent(getActivity(), SettingsActivity.class);
+            startActivity(intentActivity);
+        });
 
         btn_hansolhs.setOnClickListener(v -> {
             Intent intentURL = new Intent(Intent.ACTION_VIEW, Uri.parse(URL_HansolHS));
@@ -67,13 +78,23 @@ public class HomeFragment extends Fragment {
             startActivity(intentActivity);
         });
 
+        setTimetable();
+
+        return view;
+    }
+
+    @SuppressLint("SetTextI18n")
+    public static void setTimetable(){
+
+        String crdate = dateFormat.format(currentDate);
+
         tv_meal.setText(crdate.substring(0, 4) + "년 " +
                 crdate.substring(4, 6) + "월 " +
                 crdate.substring(6, 8) + "일\n" +
                 "급식정보\n\n" + getMealData.getMeal(crdate, "2", "메뉴"));
-        tv_timetable.setText(getTimetableData.getTimeTable(crdate, "1", "6"));
-
-        return view;
+        tv_timetable.setText(getTimetableData.getTimeTable(crdate,
+                String.valueOf(SpinnerData.spinnerGrade + 1),
+                String.valueOf(SpinnerData.spinnerClass + 1)));
     }
 
 }
