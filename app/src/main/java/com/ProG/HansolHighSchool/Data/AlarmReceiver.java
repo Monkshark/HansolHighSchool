780bffb94221dda1f83311057c2bfb20e14b160c
HuiDo Choo <manner0814@sjhansol.sjeduhs.kr>
2023-09-19 23:21:12 +0900

    Update

--- app/src/main/java/com/ProG/HansolHighSchool/Data/AlarmReceiver.java
+++ app/src/main/java/com/ProG/HansolHighSchool/Data/AlarmReceiver.java
@@ -11,17 +11,16 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class AlarmReceiver extends BroadcastReceiver {
+    @SuppressLint("SimpleDateFormat")
+    SimpleDateFormat sf = new SimpleDateFormat("yyyy-MM-dd");
+    Date currentDate = new Date();
+    String spDate = sf.format(currentDate);
 
     @Override
     public void onReceive(Context context, Intent intent) {
 
         String 분류 = intent.getStringExtra("분류");
         String 메뉴 = intent.getStringExtra("메뉴");
-
-        Date currentDate = new Date();
-        @SuppressLint("SimpleDateFormat")
-        SimpleDateFormat sf = new SimpleDateFormat("yyyy-MM-dd");
-        String spDate = sf.format(currentDate);
 
         if (분류 != null) switch (분류) {
             case "조식" -> NotificationUtil.sendNotification(context, spDate + " 조식정보", 메뉴);
