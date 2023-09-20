82ea49b332ff923e4e87e2bde5b7583bc7d5ed82
HuiDo Choo <manner0814@sjhansol.sjeduhs.kr>
2023-09-20 16:58:27 +0900

    Update

--- app/src/main/java/com/ProG/HansolHighSchool/Data/AlarmReceiver.java
+++ app/src/main/java/com/ProG/HansolHighSchool/Data/AlarmReceiver.java
@@ -9,6 +9,7 @@ import com.ProG.HansolHighSchool.API.getMealData;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Objects;
 
 public class AlarmReceiver extends BroadcastReceiver {
     @SuppressLint("SimpleDateFormat")
@@ -21,11 +22,11 @@ public class AlarmReceiver extends BroadcastReceiver {
 
         String 분류 = intent.getStringExtra("분류");
 
-        assert 분류 != null;
-        String 메뉴 = switch (분류) {
+        String 메뉴 = switch (Objects.requireNonNull(분류)) {
                 case "조식" -> getMealData.getMeal(spDate, "1", "메뉴");
                 case "중식" -> getMealData.getMeal(spDate, "2", "메뉴");
                 case "석식" -> getMealData.getMeal(spDate, "3", "메뉴");
+                default -> "";
             };
 
     }
